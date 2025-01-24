90a06e85c87b4ab1498e3b1903625d08a88c20e4
Vince Kevin Añana <152976699+mantequilla45@users.noreply.github.com>
2025-01-25 01:46:11 +0800

    Update ourservices.tsx

--- src/app/components/homepage/ourservices.tsx
+++ src/app/components/homepage/ourservices.tsx
@@ -8,48 +8,48 @@ const OurServicesSection = () => {
         </h2>
         <div className="flex flex-row gap-[50px] mt-[60px]">
           <div className="flex flex-col items-center gap-[60px] w-1/3 bg-white rounded-[30px] px-[50px] py-[70px] shadow-md">
-            <div className="w-[300px] h-[300px]">
+            <div className="w-[200px] h-[200px]">
               <Image
                 src="/graphics/symptom-checker-icon.svg"
                 alt="icon 1"
-                width={300}
-                height={300}
+                width={200}
+                height={200}
                 style={{ objectFit: 'cover' }}
                 quality={100}
               />
             </div>
             <div className="flex flex-col text-xl text-black items-center">
               <h3 className="text-xl text-[#043CAA] font-semibold">Symptom Checker</h3>
-              <p className="text-base mt-3 text-justify">
+              <p className="text-base mt-3 text-justify font-light">
                 Input your symptoms to find the right cure, view search results filtered according to your symptoms, and access visual aids with explanations to better understand your condition.
               </p>
             </div>
           </div>
           <div className="flex flex-col items-center gap-[60px] w-1/3 bg-white rounded-[30px] px-[50px] py-[70px] shadow-md">
-            <div className="w-[300px] h-[300px]">
+            <div className="w-[200px] h-[200px]">
               <Image
                 src="/graphics/otc-guide-icon.svg"
                 alt="icon 2"
-                width={300}
-                height={300}
+                width={200}
+                height={200}
                 style={{ objectFit: 'cover' }}
                 quality={100}
               />
             </div>
             <div className="flex flex-col text-xl text-black items-center">
               <h3 className="text-xl text-[#043CAA] font-semibold">OTC Medication Guidance</h3>
-              <p className="text-base mt-3 text-justify">
+              <p className="text-base mt-3 text-justify font-light">
                 Find the right over-the-counter medications based on your symptoms, access detailed information on their uses, dosages, and precautions, and stay informed about potential drug interactions and contraindications.
               </p>
             </div>
           </div>
           <div className="flex flex-col items-center gap-[60px] w-1/3 bg-white rounded-[30px] px-[50px] py-[70px] shadow-md">
-            <div className="w-[300px] h-[300px]">
+            <div className="w-[200px] h-[200px]">
               <Image
                 src="/graphics/personal-ht-icon.svg"
                 alt="icon 1"
-                width={300}
-                height={300}
+                width={200}
+                height={200}
                 style={{ objectFit: 'cover' }}
                 quality={100}
               />
@@ -57,7 +57,7 @@ const OurServicesSection = () => {
 
             <div className="flex flex-col text-xl text-black items-center">
               <h3 className="text-xl text-[#043CAA] font-semibold">Personalized Health Tips</h3>
-              <p className="text-base mt-3 text-justify">
+              <p className="text-base mt-3 text-justify font-light">
                 Receive personalized health and wellness tips based on your profile and symptom history, save and track your health data over time, and get email or SMS notifications for self-care reminders.
               </p>
             </div>
